0a23b83cafdc4c8731b0a766bdc866f4409827ac
Omar El Jraouy <oel-jrao@c2r4p14.1337.ma>
2025-05-17 19:00:01 +0100

    exercices folder

--- training/exercices/ex3.c
+++ training/exercices/ex3.c
@@ -12,9 +12,9 @@ int main()
     pthread_t t1, t2;
 
     pthread_create(&t1, NULL, ping_a, NULL);
-    pthread_join(t1, NULL);
-
     pthread_create(&t2, NULL, ping_b, NULL);
+
+    pthread_join(t1, NULL);
     pthread_join(t2, NULL);
 
     return 0;
@@ -24,7 +24,7 @@ void *ping_a(__attribute__((unused))void *arg)
 {
     for (int i = 0; i < N; i++)
     {
-        printf("Ping form A");
+        printf("Ping form A\n");
         sleep(1);
     }
     return NULL;
@@ -34,7 +34,7 @@ void *ping_b(__attribute__((unused))void *arg)
 {
     for (int i = 0; i < N; i++)
     {
-        printf("Ping form B");
+        printf("Ping form B\n");
         sleep(1);
     }
     return NULL;
