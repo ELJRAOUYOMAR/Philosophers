ded1891e22821839a14a0b2be7557b8a37c3702b
root <root@DESKTOP-6JD0245.localdomain>
2025-08-23 09:45:06 +0100

    allocate in thread and free in other

--- training/alloc_free.c
+++ training/alloc_free.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-pthread_mutex_t lock;
+/*
+    allocate in one thread and free the same variable in other thread 
+*/
 void *routine1(void *arg)
 {
     int **x = (int **)arg;
 
-    pthread_mutex_lock(&lock);
     *x = malloc(sizeof(int) * 5);
     printf("Thread 1: allocated memory\n");
-    pthread_mutex_unlock(&lock);
 
     return NULL;
 }
@@ -18,14 +18,12 @@ void *routine1(void *arg)
 void *routine2(void *arg)
 {
     int **x = (int **)arg;
-    pthread_mutex_lock(&lock);
     if (*x)
     {
         free(*x);
         *x = NULL;
         printf("Thread2: freed memory\n");
     }
-    pthread_mutex_unlock(&lock);
 
     return NULL;
 }
@@ -35,14 +33,11 @@ int main()
     pthread_t t1, t2;
     int *x = NULL;
 
-    pthread_mutex_init(&lock, NULL);
-
     pthread_create(&t1, NULL, routine1, &x);
-    pthread_create(&t2, NULL, routine2, &x);
-    
     pthread_join(t1, NULL);
+    
+    pthread_create(&t2, NULL, routine2, &x);
     pthread_join(t2, NULL);
 
-    pthread_mutex_destroy(&lock);
     return 0;
 }
